890025b9d744f0459140435470de54fde856b94e
Suraj-Roy <kumarsurajroy365.5@gmail.com>
2023-10-24 19:32:03 +0530

    Hamburger Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import Logo from "../assets/Logo.png";
+import React,{useState} from "react";
 import { GrLanguage } from "react-icons/gr";
-
+import Logo from '../assets/Logo.png';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {RxCross2} from 'react-icons/rx';
 function Navbar() {
   const [IsMenuOpen,setIsMenuOpen] = useState(false);
 
   const toggleMenuOpen = () =>{
-    if(IsMenuOpen){
+    if(!IsMenuOpen){
       setIsMenuOpen(true);
     }
     else{
@@ -20,7 +21,8 @@ function Navbar() {
     { link: "Pricing", path: "pricing" },
   ];
   return (
-    <nav className="md:px-14 p-3">
+    <>
+    <nav className="md:px-14 p-3 bg-white mx-auto top-0">
       <div className="text-lg mx-auto flex items-center justify-between font-medium">
         <div className="flex items-center space-x-14">
           <a
@@ -31,7 +33,7 @@ function Navbar() {
             <span>XYZ</span>
           </a>
 
-          <ul className="md:flex space-x-12 items">
+          <ul className="hidden lg:flex space-x-12 items">
             {navItems.map(({ link, path }) => (
               <a key={link} href={path} className="block hover:text-sky-200">
                 {link}
@@ -39,8 +41,8 @@ function Navbar() {
             ))}
           </ul>
         </div>
-        <div className=" md:flex items-center space-x-12 p-2">
-          <a href="/" className="hidden md:flex items-center">
+        <div className="hidden lg:flex items-center space-x-12 p-2 justify-end">
+          <a href="/" className="hidden lg:flex items-center">
             <GrLanguage className="mr-2" />
             <span className="text-xl hover:text-slate-400">Language</span>{" "}
           </a>
@@ -50,11 +52,25 @@ function Navbar() {
         </div>
 
         {/* menu button onlydisplay in Mobile */}
-        <div className="md:hidden">
-              <button></button>
+        <div className="lg:hidden flex items-center justify-end mr-3">
+          
+              <button className=" cursor-pointer" onClick={toggleMenuOpen}>{
+                IsMenuOpen?(<RxCross2 className="w-6 h-6 "/>):(<GiHamburgerMenu className="w-6 h-6 "/>)
+              }
+              </button>
         </div>
       </div>
-    </nav>
+   </nav>
+
+   <div className={`lg:hidden px-5 space-y-4 pb-5 pt-5 bg-purple-300 ${IsMenuOpen?"block top-0 right-0 left-0":"hidden"}`}>{
+     navItems.map(({ link, path }) => (
+      <a key={link} href={path} className="block hover:text-sky-200">
+        {link}
+      </a>
+    ))}
+
+   </div>
+   </>
   );
 }
 
